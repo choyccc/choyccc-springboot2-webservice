b343b4374b24ce042a0b39797048630fc1ad8d21
choyccc <choyccc@gmail.com>
2020-12-21 00:52:54 +0900

    web package add

--- src/main/java/com/choyccc/book/springboot/web/HelloController.java
+++ src/main/java/com/choyccc/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.choyccc.book.springboot.web;
 
+import com.choyccc.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +12,16 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
 }
+
+/*
+* RequestParam
+*   외부에서 API로 넘긴 파라미터를 가져오는 어노테이션입니다.
+*   여기서는 외부에서 name (@RequestParam("name")) 이란 이름으로 넘긴 파라미터를 메소드 파라미터 name(String name)에 저장하게 됩니다.
+*
+* */
---
+++ src/main/java/com/choyccc/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.choyccc.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter // 선언된 모든 필드의 get 메소드를 생성해줌
+@RequiredArgsConstructor //선언된 모든 파이널 필드가 포함된 생성자를 생성해 줌, 파이널이 없는 생성자는 포함되지 않음
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
