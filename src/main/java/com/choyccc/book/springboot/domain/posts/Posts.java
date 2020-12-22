12de30787fdabf9e963f7a5dfda5125cd113d200
choyccc <choyccc@gmail.com>
2020-12-22 17:14:32 +0900

    web package add 20-12-21

--- src/main/java/com/choyccc/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/choyccc/book/springboot/domain/posts/Posts.java
@@ -1,4 +1,5 @@
 package com.choyccc.book.springboot.domain.posts;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @Entity
 public class Posts {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -48,6 +50,13 @@ public class Posts {
 *   테이블의 컬럼을 나타내며 굳이 선언하지 않더라도 해당 클래스의 필드는 모두 컬럼이 됨
 *   사용하는 이유는 기본값 외에 추가로 변경이 필요한 옵션이 있으면 사용
 *   문자열의 경우 VARCHAR(255)가 기본값인데 사이즈를 500으로 늘리고 싶거나(ex: title), 타입을 TEXT로 변경하고 싶거나(ex: content) 등의 경우에 사용
-*
+* (5) @NoArgsConstructor
+*   기본 생성자 자동 생성
+*   public Posts() {} 와 같은 효과
+* (6) @Getter
+*   클래스 내 모든 필드의 Getter 메소드를 자동생성
+* (7) @Builder
+*   해당 클래스의 빌더 패턴 클래스를 생성
+*   생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
 *
 * */
